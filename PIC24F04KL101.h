da1b2c8b0056dd18182d6d600c0e0e2e3c2458c6
nerobot <s.c.daglish@googlemail.com>
2018-07-17 07:19:49 +0100

    Project created

---
+++ PIC24F04KL101.h
@@ -0,0 +1,24 @@
+/* 
+ * File:   PIC24F04KL101.h
+ * Author: stevend
+ *
+ * Created on 17 July 2018, 07:18
+ */
+
+#ifndef PIC24F04KL101_H
+#define	PIC24F04KL101_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+#define PIC24F04KL101
+#define XTAL_FREQ       8000000UL           // Raw oscillator freq
+#define FCY             XTAL_FREQ / 2       // Most but not all PIC24s
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* PIC24F04KL101_H */
+
---
+++ newmainXC16.c
@@ -0,0 +1,14 @@
+/*
+ * File:   newmainXC16.c
+ * Author: stevend
+ *
+ * Created on 17 July 2018, 07:14
+ */
+
+#include "config.h"
+#include "PIC24F04KL101.h"
+#include <xc.h>
+
+int main(void) {
+    return 0;
+}
