1a68a20c10e194d2fa1f88f6963f40ee93669f74
nerobot <s.c.daglish@googlemail.com>
2018-07-17 07:32:45 +0100

    Started writing comments as @mainpage

--- newmainXC16.c
+++ newmainXC16.c
@@ -16,9 +16,29 @@ int main(void) {
     LED = 0;
     
     while(1){
+        // Flashing the LED
         LED ^= 1;
         __delay_ms(1000);
     }
     
     return 0;
 }
+
+/** @mainpage
+ * 
+ * @author Steven Daglish
+ * 
+ * @date 17 July 2018
+ * 
+ * @version 0.1
+ * 
+ * @section secOverview Overview
+ *  
+ * Prototyping of PIC24F04KL101 low power RF temperature sensor.
+ * 
+ * @section secHistory History
+ * 
+ * # 17 July 2018
+ * 
+ *  * First creation of project using the PIC24F04KL101 MCU.
+ */
